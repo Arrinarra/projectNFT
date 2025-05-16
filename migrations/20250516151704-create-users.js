f75eb16099f5c192c21e8883045a33771490dffa
Arrinarra <156569304+Arrinarra@users.noreply.github.com>
2025-05-16 22:19:22 +0700

    заполнение миграций

--- migrations/20250516151704-create-users.js
+++ migrations/20250516151704-create-users.js
@@ -1,22 +1,32 @@
-'use strict';
-
-/** @type {import('sequelize-cli').Migration} */
+// migrations/XXXXXXXXXXXXXX-create-users.js
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add altering commands here.
-     *
-     * Example:
-     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
-     */
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
   },
-
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add reverting commands here.
-     *
-     * Example:
-     * await queryInterface.dropTable('users');
-     */
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
   }
 };
