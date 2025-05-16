19c19c03398535bd8ba2dc41bbb7ceb1d821a319
Arrinarra <156569304+Arrinarra@users.noreply.github.com>
2025-05-16 23:02:57 +0700

    создание ЕБАНОЙ бд

---
+++ backend/src/migrations/20250516151704-create-users.js
@@ -0,0 +1,32 @@
+// migrations/XXXXXXXXXXXXXX-create-users.js
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        type: Sequelize.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      createdAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      },
+      updatedAt: {
+        type: Sequelize.DATE,
+        allowNull: false
+      }
+    });
+  },
+  async down(queryInterface) {
+    await queryInterface.dropTable('Users');
+  }
+};
