d4710afac430dde0756c35f4e0b3ecc7be025839
Arrinarra <156569304+Arrinarra@users.noreply.github.com>
2025-05-16 17:31:49 +0700

    добавление тестового роута

--- src/index.js
+++ src/index.js
@@ -8,6 +8,10 @@ const User = require('./models/User');
 const app = express();
 const PORT = process.env.PORT || 5000;
 
+app.get('/', (req, res) => {
+  res.send('Backend is working!');
+});
+
 // Middleware
 app.use(cors());
 app.use(express.json());
