0a29cda2c0b3ef26c4987f57c81c2a58a0523d78
Arrinarra <156569304+Arrinarra@users.noreply.github.com>
2025-05-16 22:10:44 +0700

    пересоздание репозитория

--- backend/src/config/db.js
+++
@@ -1,9 +0,0 @@
-const { Sequelize } = require('sequelize');
-require('dotenv').config();
-
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './dev.sqlite', // Путь к файлу БД
-});
-
-module.exports = sequelize;
